5e89e60de9bad1d5b9d17e282961cd5970644222
Tsukune83t <nico83t@gmail.com>
2019-04-07 21:01:38 +0200

    finish react3

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { Button } from 'reactstrap';
 import "./App.css";
-
-import Lamp from "./Lamp"
 import Quotes from "./Quotes";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        working: false
+    };
+}
   render() {
+    const imgClass = this.state.working ? 'working' : 'breaking' ;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <div className="container">
+          <img src="https://www.logolynx.com/images/logolynx/b3/b37db0039b7487697d2aadbd3b49925c.gif" className={imgClass} alt="logo" />
           <h1 className="App-title">Simpsons Quotes</h1>
-        </header>
-        <Lamp on />
-        <Lamp />
+        </div><br />
+        <div>
+        <Button onClick={() => 
+          {this.setState({ working: !this.state.working});}} outline color="danger">WORK/BREAK</Button>{' '}        
+        </div>
+        
         <Quotes  />
       </div>
     );
